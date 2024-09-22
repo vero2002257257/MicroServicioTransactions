0c9f3c90e4e66d15da7fd8479c725fa057c64f54
veronica <105020892+vero2002257257@users.noreply.github.com>
2024-09-22 18:33:07 -0500

    Creacion microservicio transacciones

---
+++ src/main/java/com/example/E_mazon/MicroservicioTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_mazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/E_mazon/MicroservicioTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_mazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
